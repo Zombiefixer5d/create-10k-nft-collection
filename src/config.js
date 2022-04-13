672ce8752d66bf9428202e51aa91f793be5f6494
Zombiefixer5d <102944921+Zombiefixer5d@users.noreply.github.com>
2022-04-12 21:53:22 -0400

    enter

--- src/config.js
+++ src/config.js
@@ -13,7 +13,7 @@ const baseUri = "ipfs://NewUriToReplace"; // This will be replaced automatically
 // If you have selected Solana then the collection starts from 0 automatically
 const layerConfigurations = [
   {
-    growEditionSizeTo: 20000,
+    growEditionSizeTo: 500,
     layersOrder: [
       { name: "Background" },
       { name: "Background Bottom Left" },
